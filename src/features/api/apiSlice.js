63d8c53f9f506b333e146c6bb4ac9f817554fc0e
samiulalimsaad <saadnbiuraj@gmail.com>
2022-09-08 19:19:20 +0600

    `8.5` added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,13 +3,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4000/",
+        baseUrl: "http://localhost:4000",
     }),
     endpoints: (builder) => ({
         getVideos: builder.query({
             query: () => "/videos",
         }),
+        getVideo: builder.query({
+            query: (id) => `/videos/${id}`,
+        }),
     }),
 });
 
-export const { useGetVideosQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery } = apiSlice;
